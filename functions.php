4c89c4533e73a45a513935dde22038befbc9a09f
wpsolo-david <65789336+wpsolo-david@users.noreply.github.com>
2020-05-22 22:56:17 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,3 @@
+/** Disable Ajax Call from WooCommerce */
+add_action( 'wp_enqueue_scripts', 'dequeue_woocommerce_cart_fragments', 11); 
+function dequeue_woocommerce_cart_fragments() { if (is_front_page()) wp_dequeue_script('wc-cart-fragments'); }
